9d09259117a20f68b76acb21dd941685d47f4c3a
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-15 17:56:11 +0800

    setup websocket

--- server/main.go
+++ server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// Allow all connections for now
+		return true
+	},
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+			log.Println(err)
+			return
+	}
+	defer conn.Close()
+
+	for {
+    messageType, p, err := conn.ReadMessage()
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    if err := conn.WriteMessage(messageType, p); err != nil {
+        log.Println(err)
+        return
+    }
+	}
+}
+
 
 func main() {
 	fmt.Println("Hello")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
