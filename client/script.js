151c2a1169de0f0ed76a59dfc2ce2c87a7fcc7c4
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-16 10:04:30 +0800

    setup client websocket

--- client/script.js
+++ client/script.js
@@ -1,1 +1,18 @@
-console.log("test")
+const socket = new WebSocket("ws://localhost:8080/");
+
+socket.onopen = function(event) {
+  console.log("WebSocket connection established.");
+  socket.send("Hello, server!");
+};
+
+socket.onmessage = function(event) {
+  console.log("Message from server:", event.data);
+};
+
+socket.onerror = function(error) {
+  console.error("WebSocket error:", error);
+};
+
+socket.onclose = function(event) {
+  console.log("WebSocket connection closed.");
+};
